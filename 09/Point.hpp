91b275d85f8e48564d6027d14991e9d9d322cb5d
yushkov-i <47275068+yushkov-i@users.noreply.github.com>
2019-04-20 20:40:43 +0300

    Update Point.hpp

--- 09/Point.hpp
+++ 09/Point.hpp
@@ -3,17 +3,26 @@
 
 namespace my
 {
+	/*
+		Пользовательский тип данных, описывает точку на плоскости
+	*/
 	struct Point
 	{
 		float x;
 		float y;
 	};
 
+	/*
+		Пользовательский тип данных, указавает на квадрант, в котором может лежать
+	*/
 	enum Quadrant
 	{
 		First, Second, Third, Fourth
 	};
 
+	/*
+		Функция, вычисляет расстояние между двумя точками
+	*/
 	float getDistanceTo(Point first, Point second)
 	{
 		return sqrt(
@@ -22,11 +31,18 @@ namespace my
 		);
 	}
 
+	/*
+		Функция, возвращает точку с "противоположными" координатами
+		Например, для точки с координатами (2,3) функция вернет точку (-2,-3)
+	*/
 	Point getAntipodePoint(Point point)
 	{
 		return Point { -point.x, -point.y };		
 	}
 
+	/*
+		Функция, возвращает квадрант передаваемой точки
+	*/
 	Quadrant getQuadrant(Point point)
 	{
 		if (point.x >= 0 && point.y >= 0)
@@ -47,6 +63,9 @@ namespace my
 		}
 	}
 
+	/*
+		Функция, меняет местами значение координат точек
+	*/
 	void swap(Point * lhs, Point * rhs)
 	{
 		Point temp = *lhs;
